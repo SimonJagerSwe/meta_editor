dea9c8e15f3097d68383b6bc8990ea3ad836acc9
Simon Jäger <simonjager@hotmail.com>
2026-03-24 23:29:07 +0100

    hello

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 # Imports
+import argparse
 import os
 import sys
 
 # Constants
 
 # Args
+
+# Main function
+def main():
+    print("Hello, editor!")
+
+
+# Execute main function if main
+if __name__ == "__main__":
+    main()
